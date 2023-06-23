fcc8617aa9adc51f1635aa4e458e819d9993c238
Jonathan Steele <iNoles@users.noreply.github.com>
2023-06-22 20:45:47 -0400

    Initial Commit

---
+++ StudentReports/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StudentReports
+//
+//  Created by Jonathan Steele on 6/22/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
