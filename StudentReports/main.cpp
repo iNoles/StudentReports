b88823e4449b24c680f18990dcd226cd3e033b1f
Jonathan Steele <iNoles@users.noreply.github.com>
2024-06-04 19:42:25 -0400

    Update main.cpp

--- StudentReports/main.cpp
+++ StudentReports/main.cpp
@@ -1,4 +1,3 @@
-//
 //  main.cpp
 //  StudentReports
 //
@@ -7,19 +6,19 @@
 
 #include "students.hpp"
 #include <iostream>
-#include<fstream>
+#include <fstream>
 
 using namespace std;
 
-//function declaration
+// Function declarations
 void displayMenu();
 void create_student();
-void display_sp(int);//display particular record
-void display_all(); // display all records
-void delete_student(int);//delete particular record
-void change_student(int);//edit particular record
+void display_sp(int); // Display particular record
+void display_all();   // Display all records
+void delete_student(int); // Delete particular record
+void change_student(int); // Edit particular record
 
-int main(int argc, const char * argv[]) {
+int main() {
     displayMenu();
     return 0;
 }
@@ -27,162 +26,164 @@ int main(int argc, const char * argv[]) {
 void displayMenu() {
     char ch;
     int num;
-    cout<<"\n\n\n\tMENU";
-    cout<<"\n\n\t1.Create student record";
-    cout<<"\n\n\t2. Search student record";
-    cout<<"\n\n\t3. Display all students records ";
-    cout<<"\n\n\t4.Delete student record";
-    cout<<"\n\n\t5.Modify student record";
-    cout<<"\n\n\t6.Exit";
-    cout<<"\n\n\tWhat is your Choice (1/2/3/4/5/6)\n";
-    cin>>ch;
-    switch(ch) {
-        case '1':
-            create_student();
-            displayMenu();
-            break;
-        case '2':
-            cout<<"\n\n\tEnter The roll number ";
-            cin>>num;
-            display_sp(num);
-            displayMenu();
-            break;
-        case '3':
-            display_all();
-            break;
-        case '4':
-            cout<<"\n\n\tEnter The roll number: ";
-            cin>>num;
-            delete_student(num);
-            displayMenu();
-            break;
-        case '5':
-            cout<<"\n\n\tEnter The roll number ";
-            cin>>num;
-            change_student(num);
-            displayMenu();
-            break;
-        case '6':
-            cout<<"Exiting, Thank you!";
-            exit(0);
-    }
+    do {
+        cout << "\n\n\n\tMENU";
+        cout << "\n\n\t1. Create student record";
+        cout << "\n\n\t2. Search student record";
+        cout << "\n\n\t3. Display all student records";
+        cout << "\n\n\t4. Delete student record";
+        cout << "\n\n\t5. Modify student record";
+        cout << "\n\n\t6. Exit";
+        cout << "\n\n\tWhat is your choice (1/2/3/4/5/6)? ";
+        cin >> ch;
+
+        switch (ch) {
+            case '1':
+                create_student();
+                break;
+            case '2':
+                cout << "\n\n\tEnter the roll number: ";
+                cin >> num;
+                display_sp(num);
+                break;
+            case '3':
+                display_all();
+                break;
+            case '4':
+                cout << "\n\n\tEnter the roll number: ";
+                cin >> num;
+                delete_student(num);
+                break;
+            case '5':
+                cout << "\n\n\tEnter the roll number: ";
+                cin >> num;
+                change_student(num);
+                break;
+            case '6':
+                cout << "Exiting, thank you!";
+                break;
+            default:
+                cout << "\n\n\tInvalid choice! Please try again.";
+        }
+    } while (ch != '6');
 }
 
-//write student details to file
+// Write student details to file
 void create_student() {
     student stud;
-    ofstream oFile;
-    oFile.open("student.dat",ios::binary|ios::app);
-    stud.getdata();
-    oFile.write(reinterpret_cast<char *> (&stud), sizeof(student));
-    oFile.close();
-    cout<<"\n\nStudent record Has Been Created ";
-    cin.ignore();
-    cin.get();
-}
-
-// read file records
-void display_all() {
-    student stud;
-    ifstream inFile;
-    inFile.open("student.dat",ios::binary);
-    if(!inFile) {
-        cout<<"File could not be opened !! Press any Key to exit";
+    ofstream oFile("student.dat", ios::binary | ios::app);
+    if (!oFile) {
+        cout << "File could not be opened! Press any key to exit...";
         cin.ignore();
         cin.get();
         return;
     }
-    cout<<"\n\n\n\t\tDISPLAYING ALL RECORDS\n\n";
-    while(inFile.read(reinterpret_cast<char *> (&stud), sizeof(student))) {
+    stud.getdata();
+    oFile.write(reinterpret_cast<char*>(&stud), sizeof(student));
+    oFile.close();
+    cout << "\n\nStudent record has been created.";
+    cin.ignore();
+    cin.get();
+}
+
+// Read and display all records
+void display_all() {
+    student stud;
+    ifstream inFile("student.dat", ios::binary);
+    if (!inFile) {
+        cout << "File could not be opened! Press any key to exit...";
+        cin.ignore();
+        cin.get();
+        return;
+    }
+    cout << "\n\n\n\t\tDISPLAYING ALL RECORDS\n\n";
+    while (inFile.read(reinterpret_cast<char*>(&stud), sizeof(student))) {
         stud.showdata();
-        cout<<"\n\n====================================\n";
+        cout << "\n\n====================================\n";
     }
     inFile.close();
     cin.ignore();
     cin.get();
 }
 
-//read specific record based on roll number
+// Read and display specific record based on roll number
 void display_sp(int n) {
     student stud;
-    ifstream iFile;
-    iFile.open("student.dat",ios::binary);
-    if(!iFile) {
-        cout<<"File could not be opened... Press any Key to exit";
+    ifstream iFile("student.dat", ios::binary);
+    if (!iFile) {
+        cout << "File could not be opened! Press any key to exit...";
         cin.ignore();
         cin.get();
         return;
     }
-    bool flag=false;
-    while(iFile.read(reinterpret_cast<char *> (&stud), sizeof(student))) {
-        if(stud.retrollno()==n) {
+    bool found = false;
+    while (iFile.read(reinterpret_cast<char*>(&stud), sizeof(student))) {
+        if (stud.retrollno() == n) {
             stud.showdata();
-            flag=true;
+            found = true;
         }
     }
     iFile.close();
-    if(flag==false)
-        cout<<"\n\nrecord does not exist";
+    if (!found) {
+        cout << "\n\nRecord does not exist.";
+    }
     cin.ignore();
     cin.get();
 }
 
-// modify record for specified roll number
+// Modify record for specified roll number
 void change_student(int n) {
-    bool found=false;
+    bool found = false;
     student stud;
-    fstream fl;
-    fl.open("student.dat",ios::binary|ios::in|ios::out);
-    if(!fl) {
-        cout<<"File could not be opened. Press any Key to exit...";
+    fstream fl("student.dat", ios::binary | ios::in | ios::out);
+    if (!fl) {
+        cout << "File could not be opened! Press any key to exit...";
         cin.ignore();
         cin.get();
         return;
     }
-    while(!fl.eof() && found==false) {
-        fl.read(reinterpret_cast<char *> (&stud), sizeof(student));
-        if(stud.retrollno()==n) {
+    while (!fl.eof() && !found) {
+        fl.read(reinterpret_cast<char*>(&stud), sizeof(student));
+        if (stud.retrollno() == n) {
             stud.showdata();
-            cout<<"\n\Enter new student details:"<<endl;
+            cout << "\n\nEnter new student details:\n";
             stud.getdata();
-            int pos=(-1)*static_cast<int>(sizeof(stud));
-            fl.seekp(pos,ios::cur);
-            fl.write(reinterpret_cast<char *> (&stud), sizeof(student));
-            cout<<"\n\n\t Record Updated";
-            found=true;
+            int pos = static_cast<int>(fl.tellp()) - sizeof(stud);
+            fl.seekp(pos, ios::beg);
+            fl.write(reinterpret_cast<char*>(&stud), sizeof(student));
+            cout << "\n\n\tRecord updated.";
+            found = true;
         }
     }
     fl.close();
-    if(found==false)
-        cout<<"\n\n Record Not Found ";
+    if (!found) {
+        cout << "\n\nRecord not found.";
+    }
     cin.ignore();
     cin.get();
 }
 
-//delete record with particular roll number
+// Delete record with particular roll number
 void delete_student(int n) {
     student stud;
-    ifstream iFile;
-    iFile.open("student.dat",ios::binary);
-    if(!iFile) {
-        cout<<"File could not be opened... Press any Key to exit...";
+    ifstream iFile("student.dat", ios::binary);
+    if (!iFile) {
+        cout << "File could not be opened! Press any key to exit...";
         cin.ignore();
         cin.get();
         return;
     }
-    ofstream oFile;
-    oFile.open("Temp.dat",ios::out);
-    iFile.seekg(0,ios::beg);
-    while(iFile.read(reinterpret_cast<char *> (&stud), sizeof(student))) {
-        if(stud.retrollno()!=n) {
-            oFile.write(reinterpret_cast<char *> (&stud), sizeof(student));
+    ofstream oFile("Temp.dat", ios::binary);
+    while (iFile.read(reinterpret_cast<char*>(&stud), sizeof(student))) {
+        if (stud.retrollno() != n) {
+            oFile.write(reinterpret_cast<char*>(&stud), sizeof(student));
         }
     }
     oFile.close();
     iFile.close();
     remove("student.dat");
-    rename("Temp.dat","student.dat");
-    cout<<"\n\n\tRecord Deleted ..";
+    rename("Temp.dat", "student.dat");
+    cout << "\n\n\tRecord deleted.";
     cin.ignore();
     cin.get();
 }
