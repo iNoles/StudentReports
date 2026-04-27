ebe099761475258ef553bfe7e45c832031bb6846
Jonathan Steele <iNoles@users.noreply.github.com>
2026-04-27 12:55:54 -0400

    Add getGrade method to Student class

--- students.hpp
+++ students.hpp
@@ -21,6 +21,7 @@ public:
     void showData() const;
     int getRollNo() const;
     double getAverage() const;
+    char getGrade() const;
 
     bool updateMarks(int eng, int math, int sci, int lang2, int cs);
     static void displayAll(const std::vector<Student>& students);
